e942490dfbbe820321225c241b171da76e77af25
Florin Iacob <florin.iacob@gmail.com>
2026-01-10 19:50:37 +0200

    Improve SendOrderDialogComponent.

--- src/components/transactions/send-order-dialog.component.tsx
+++ src/components/transactions/send-order-dialog.component.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import {IStrategyViewModel} from "../../models/strategy.view-model.interface";
+import {IStrategySendOrderParams, IStrategyViewModel} from "../../models/strategy.view-model.interface";
 import {IonButton, IonIcon, IonModal} from "@ionic/react";
 import {observer} from "mobx-react";
 import styled from "styled-components";
 import {InputBaseBox} from "../input-base.box";
-import {closeOutline} from "ionicons/icons";
+import {closeOutline, lockClosedOutline, lockOpenOutline} from "ionicons/icons";
 import {OrderType, TimeInForce} from "../../services/broker-account/broker-account.service.interface";
+import {NullableString} from "../../utils/nullable-types";
+import {Check} from "../../utils/type-checking";
 
 const ContentBox = styled.div`
     display: flex;
@@ -35,13 +37,20 @@ const BodyBox = styled.div`
     flex-grow: 1;
     justify-content: center;
     justify-items: center;
-    
+`
+
+const RowSeparatorBox = styled.div`
+    grid-column: 1/-1;
+    width: 100%;
+    height: 1px;
+    background-color: var(--ion-color-light-shade);
 `
 
 const FieldsGridBox = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr;
-    row-gap: 16px;
+    grid-template-columns: 1.5fr 1fr;
+    gap: 16px;
+    
 `
 
 const PriceBox = styled(InputBaseBox)`
@@ -74,6 +83,21 @@ const FieldLabelBox = styled.div`
     justify-content: center;
 `
 
+const ReadonlyFieldValueBox = styled(FieldLabelBox)`
+    padding-left: 8px;
+`
+
+const TargetPriceBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 4px;
+`
+
+const PriceLockerBox = styled.div`
+    cursor: pointer;
+    font-size: 1.3rem;
+`
 
 interface SendOrderDialogComponentProps {
     isOpen: boolean;
@@ -82,18 +106,63 @@ interface SendOrderDialogComponentProps {
 }
 
 export const SendOrderDialogComponent: React.FC<SendOrderDialogComponentProps> = observer((props) => {
+    const [targetPrice, setTargetPrice] = React.useState<NullableString>(null);
     const [quantity, setQuantity] = React.useState<number>(1);
-    const [orderType, setOrderType] = React.useState<OrderType>("Limit");
-    const [timeInForce, setTimeInForce] = React.useState<TimeInForce>("Day");
+    const [orderType] = React.useState<OrderType>("Limit");
+    const [timeInForce] = React.useState<TimeInForce>("Day");
 
     const sendOrder = async () => {
-        await props.strategy.sendOrder({
+        const orderParams: IStrategySendOrderParams = {
             quantity: quantity,
             orderType: orderType,
             timeInForce: timeInForce
-        });
+        }
+
+        if(targetPrice) {
+            orderParams.price = parseFloat(targetPrice);
+        }
+
+        await props.strategy.sendOrder(orderParams);
         props.onDitDismiss();
     }
+
+    const onPriceChanged = (value: string) => {
+        const p = parseFloat(value);
+        if(!Check.isNumber(p)) {
+            setTargetPrice(null);
+            return;
+        }
+        setTargetPrice(value);
+    }
+
+    const onQuantityChange = (value: string) => {
+        const q = parseInt(value);
+        if(!Check.isNumber(q)) {
+            return;
+        }
+        setQuantity(Math.max(1, q));
+    }
+
+    const tradePrice = targetPrice ?? props.strategy.credit.toString();
+
+    const isPriceLocked = Boolean(targetPrice);
+
+    const renderLockerIcon = () => {
+        if(isPriceLocked) {
+            return <IonIcon icon={lockClosedOutline}/>
+        } else {
+            return <IonIcon icon={lockOpenOutline}/>
+        }
+    }
+
+    const onLockerClick = () => {
+        if(isPriceLocked) {
+            setTargetPrice(null);
+        } else {
+            setTargetPrice(props.strategy.credit.toString());
+        }
+    }
+
     return (
         <IonModal isOpen={props.isOpen} onDidDismiss={props.onDitDismiss}>
             <ContentBox>
@@ -107,14 +176,35 @@ export const SendOrderDialogComponent: React.FC<SendOrderDialogComponentProps> =
                 </HeaderBox>
                 <BodyBox>
                     <FieldsGridBox>
-                        <FieldLabelBox>Price (readonly for the moment)</FieldLabelBox>
-                        <PriceBox value={props.strategy.credit} readOnly={true}/>
+                        <FieldLabelBox>Current price</FieldLabelBox>
+                        <ReadonlyFieldValueBox>
+                            {props.strategy.credit}
+                        </ReadonlyFieldValueBox>
+
+                        <RowSeparatorBox/>
+
+                        <FieldLabelBox>Trade price</FieldLabelBox>
+                        <TargetPriceBox>
+                            <PriceBox value={tradePrice} onChange={e => onPriceChanged(e.target.value)}/>
+                            <PriceLockerBox onClick={onLockerClick}>
+                                {renderLockerIcon()}
+                            </PriceLockerBox>
+                        </TargetPriceBox>
+
+                        <RowSeparatorBox/>
+
                         <FieldLabelBox>Quantity</FieldLabelBox>
-                        <QuantityBox type={"number"} value={quantity} onChange={e => setQuantity(Math.max(1, parseInt(e.target.value)))}/>
+                        <QuantityBox type={"number"} value={quantity} onChange={e => onQuantityChange(e.target.value)}/>
+
+                        <RowSeparatorBox/>
+
                         <FieldLabelBox>Order Type (readonly for the moment)</FieldLabelBox>
-                        <FieldLabelBox>{orderType}</FieldLabelBox>
+                        <ReadonlyFieldValueBox>{orderType}</ReadonlyFieldValueBox>
+
+                        <RowSeparatorBox/>
+
                         <FieldLabelBox>Time in force (readonly for the moment)</FieldLabelBox>
-                        <FieldLabelBox>{timeInForce}</FieldLabelBox>
+                        <ReadonlyFieldValueBox>{timeInForce}</ReadonlyFieldValueBox>
                     </FieldsGridBox>
 
                 </BodyBox>
