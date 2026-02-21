4a112c43f2e71df5432eff48bee9fcac00a31d82
Florin Iacob <florin.iacob@gmail.com>
2026-02-21 18:11:37 +0200

    Remove ExplorerContainer component.

--- src/app/pages/strategies/strategies.page.tsx
+++ src/app/pages/strategies/strategies.page.tsx
@@ -1,12 +1,25 @@
 import React from "react";
-import ExploreContainer from '../../components/ExploreContainer';
 import {observer} from "mobx-react-lite";
 import {TastyScannerStandardPage} from "../tasty-scanner-standard.page";
+import {TickerOptionsStrategiesComponent} from "./components/ticker-options-strategies.component";
+import styled from "styled-components";
+
+const ContainerBox = styled.div`
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+    padding: 24px;
+`
 
 export const StrategiesPage: React.FC = observer(() => {
     return (
         <TastyScannerStandardPage>
-            <ExploreContainer />
+            <ContainerBox>
+                <TickerOptionsStrategiesComponent/>
+            </ContainerBox>
         </TastyScannerStandardPage>
     )
 });
