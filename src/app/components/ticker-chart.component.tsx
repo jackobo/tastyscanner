9195fd9452dbc27640a9ed3e9d2d58bbfc86c309
Florin Iacob <florin.iacob@gmail.com>
2026-02-21 14:16:05 +0200

    Adds NoTickerSelectedBox

--- src/app/components/ticker-chart.component.tsx
+++ src/app/components/ticker-chart.component.tsx
@@ -3,20 +3,31 @@ import {ITickerViewModel} from "../models/ticker.view-model.interface";
 import {TradingViewWidgetComponent} from "./trading-view-widget.component";
 import {observer} from "mobx-react";
 import {useServices} from "../hooks/use-services.hook";
+import styled from "styled-components";
 
+const NoTickerSelectedBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    color: var(--ion-color-danger);
+    width: 100%;
+    height: 100%;
+`
 
 
 export const TickerChartComponent: React.FC<{ticker: ITickerViewModel | null}> = observer((props) => {
     const services = useServices();
     if(!props.ticker) {
         return (
-            <div>
+            <NoTickerSelectedBox>
                 {services.language.translate('No ticker selected.')}
-            </div>
+            </NoTickerSelectedBox>
         );
     }
     return (
         <TradingViewWidgetComponent symbol={props.ticker.symbol} listedMarket={props.ticker.listedMarket}/>
     )
+
 
 })
