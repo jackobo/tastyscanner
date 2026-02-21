7421ae9636704f62a46ff1cad6158aa259f30951
Florin Iacob <florin.iacob@gmail.com>
2026-02-21 14:09:11 +0200

    Refactor TickerChartComponent

--- src/app/components/ticker-chart.component.tsx
+++ src/app/components/ticker-chart.component.tsx
@@ -1,37 +1,22 @@
-import React, {useState} from "react";
+import React from "react";
 import {ITickerViewModel} from "../models/ticker.view-model.interface";
-import {IonAccordion, IonAccordionGroup, IonItem} from "@ionic/react";
 import {TradingViewWidgetComponent} from "./trading-view-widget.component";
-import styled from "styled-components";
 import {observer} from "mobx-react";
-import {Check} from "../../framework/utils/type-checking";
+import {useServices} from "../hooks/use-services.hook";
 
-const TickerChartHeaderBox = styled(IonItem)`
-    cursor: pointer;
-    --background: var(--ion-color-light-shade);
-    --color: var(--ion-color-light-contrast);
-`
-const TickerChartContainerBox = styled.div`
-    height: calc(100vh - 200px);
-`
 
 
 export const TickerChartComponent: React.FC<{ticker: ITickerViewModel | null}> = observer((props) => {
-    const [isExpanded, setIsExpanded] = useState(false);
+    const services = useServices();
     if(!props.ticker) {
-        return null;
+        return (
+            <div>
+                {services.language.translate('No ticker selected.')}
+            </div>
+        );
     }
     return (
-        <IonAccordionGroup onIonChange={(e) => {
-            setIsExpanded(!Check.isNullOrUndefined(e.detail.value));
-        }}>
-            <IonAccordion>
-                <TickerChartHeaderBox slot="header">Chart</TickerChartHeaderBox>
-                <TickerChartContainerBox slot="content">
-                    {isExpanded && <TradingViewWidgetComponent symbol={props.ticker.symbol} listedMarket={props.ticker.listedMarket}/>}
-                </TickerChartContainerBox>
-            </IonAccordion>
-        </IonAccordionGroup>
+        <TradingViewWidgetComponent symbol={props.ticker.symbol} listedMarket={props.ticker.listedMarket}/>
     )
 
 })
