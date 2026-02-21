f740bb7a2ca8041425a0940a9e471140a232827e
Florin Iacob <florin.iacob@gmail.com>
2026-02-21 10:19:46 +0200

    Show check mark next to the current open side menu.

--- src/app/components/side-menu/right-side-trigger.menu-item.component.tsx
+++ src/app/components/side-menu/right-side-trigger.menu-item.component.tsx
@@ -6,11 +6,54 @@ import {
 import {
     IRightSideTriggerMenuItemViewModel
 } from "../../services/side-menu/left/models/right-side-trigger.menu-item.view-model.interface";
+import styled from "styled-components";
+import {IonIcon} from "@ionic/react";
+import {checkmarkOutline} from "ionicons/icons";
+
+const MenuItemContentContainerBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+`
+
+const MenuItemMainContentBox = styled.div`
+    flex-grow: 1;
+`
+
+const CheckIconBox = styled.div`
+    color: var(--ion-color-success-shade);
+`
+
 
 export const RightSideTriggerMenuItemComponent: React.FC<{menuItem: IRightSideTriggerMenuItemViewModel}> = observer((props) => {
+
+    const renderCheckIcon = () => {
+        if(!props.menuItem.isOpen) {
+            return null;
+        }
+
+        return (
+            <CheckIconBox>
+                <IonIcon icon={checkmarkOutline}/>
+            </CheckIconBox>
+        )
+    }
+
+    const renderContent = () => {
+        return (
+            <MenuItemContentContainerBox>
+                <MenuItemMainContentBox>
+                    {props.menuItem.renderMenuItemContent()}
+                </MenuItemMainContentBox>
+                {renderCheckIcon()}
+            </MenuItemContentContainerBox>
+        )
+    }
+
     return (
         <StandardSideMenuItemComponent renderIcon={() => props.menuItem.renderIcon()}
-                                       renderContent={() => props.menuItem.renderMenuItemContent()}
+                                       renderContent={renderContent}
                                        isSelected={() => false}
                                        onClick={() => props.menuItem.open()}/>
     )
