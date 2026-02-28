9ae0f75f153574fec4e76220d461b8f8ee905e53
Florin Iacob <florin.iacob@gmail.com>
2026-02-28 17:00:30 +0200

    Extract some components in their own files

---
+++ src/app/pages/open-positions/boxes/grid-body.boxes.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import {COMMON_COLUMNS_TEMPLATE, LEG_INFO_WIDTH} from "./constants";
+import {GridCellBox} from "./common.boxes";
+
+export const BodyGridBox = styled.div`
+    display: grid;
+    grid-template-columns: ${LEG_INFO_WIDTH} ${COMMON_COLUMNS_TEMPLATE};
+    align-items: center;
+`
+
+export const GridBodyCellBox = styled(GridCellBox)`
+    border-bottom: 1px solid var(--ion-color-border);
+`
+
+export const RightAlignedBodyGridCellBox = styled(GridBodyCellBox)`
+    text-align: right;
+`
+
+export const CenterAlignedBodyGridCellBox = styled(GridBodyCellBox)`
+    text-align: center;
+`
+
+export const LegInfoGridCellBox = styled.div`
+    width: 100%;
+    text-align: center;
+`
+
+
+export const LegInfoBodyGridCellBox = styled(GridBodyCellBox)`
+    display: grid;
+    grid-template-columns: 25px auto 10px auto auto auto auto;
+    flex-direction: row;
+    align-items: center;
+    justify-items: center;
+    justify-content: space-evenly;
+    gap: var(--ion-space-8);
+    background-color: var(--ion-color-light);
+    color: var(--ion-color-light-contrast);
+    
+`
--- src/app/pages/open-positions/open-positions.page.tsx
+++ src/app/pages/open-positions/open-positions.page.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import {observer} from "mobx-react";
 import {TastyScannerStandardPage} from "../tasty-scanner-standard.page";
 import {useServices} from "../../hooks/use-services.hook";
-import {
-    IAccountOpenOrderLegViewModel,
-    IAccountOpenOrderViewModel
-} from "../../services/brokers/interfaces/account-open-order-interface";
 import styled from "styled-components";
 import {SpinnerComponent} from "../../../framework/components/spinner/spinner.component";
-import {IonAccordion, IonAccordionGroup, IonItem} from "@ionic/react";
+import {IonAccordionGroup} from "@ionic/react";
 
-const LEG_INFO_CELL_WIDTH = '210px';
+import {HeaderComponent} from "./components/open-positions-header.component";
+import {UnderlyingSymbolOpenOrdersComponent} from "./components/underlygin-symbol-open-orders.component";
 
 const PAGE_CONTENT_CSS_CLASS = 'open-positions-page-content'
 
@@ -21,183 +18,12 @@ const PageBox = styled(TastyScannerStandardPage)`
     }
 `
 
-
 const PageContentBox = styled.div`
     width: 100%;
     flex-grow: 1;
     max-height: 100%;
     overflow-y: auto;
 `
-
-const SymbolBox = styled(IonItem)`
-    cursor: pointer;
-    font-weight: var(--ion-font-weight-bold);
-    --background: var(--ion-color-light);
-    --color: var(--ion-color-light-contrast);
-    
-`
-
-const OrdersBox = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: var(--ion-space-8);
-    padding-left: var(--ion-space-24);
-    padding-bottom: var(--ion-space-20);
-`
-
-const HeaderGridBox = styled.div`
-    display: grid;
-    grid-template-columns: calc(${LEG_INFO_CELL_WIDTH} + var(--ion-space-24)) 1fr 1fr 1fr 1fr;
-    align-items: center;
-    width: 100%;
-    font-size: var(--ion-font-size-h6);
-`
-
-
-const BodyGridBox = styled.div`
-    display: grid;
-    grid-template-columns: ${LEG_INFO_CELL_WIDTH} 1fr 1fr 1fr 1fr;
-    align-items: center;
-`
-
-const GridCellBox = styled.div`
-    padding: var(--ion-space-8);
-`
-
-const GridBodyCellBox = styled(GridCellBox)`
-    border-bottom: 1px solid var(--ion-color-border);
-`
-
-const GridHeaderCellBox = styled(GridCellBox)`
-    font-weight: var(--ion-font-weight-bold);
-`
-
-const RightBodyGridCellBox = styled(GridBodyCellBox)`
-    text-align: right;
-`
-
-const CenteredBodyGridCellBox = styled(GridBodyCellBox)`
-    text-align: center;
-`
-
-const RightHeaderGridCellBox = styled(GridHeaderCellBox)`
-    text-align: right;
-`
-
-const CenteredHeaderGridCellBox = styled(GridHeaderCellBox)`
-    text-align: center;
-`
-
-const LegInfoHeaderGridBox = styled(GridHeaderCellBox)`
-`
-
-const LegInfoBodyGridBox = styled(GridBodyCellBox)`
-    display: grid;
-    grid-template-columns: 25px auto 10px auto auto auto auto;
-    flex-direction: row;
-    align-items: center;
-    justify-items: center;
-    justify-content: space-evenly;
-    gap: var(--ion-space-8);
-    background-color: var(--ion-color-light);
-    color: var(--ion-color-light-contrast);
-    
-`
-
-const LegInfoGridCellBox = styled.div`
-    width: 100%;
-    text-align: center;
-`
-
-const LegInfoSeparatorBox = styled.span`
-    display: flex;
-    height: 70%;
-    border-right: 1px solid var(--ion-color-dark);
-`
-
-const OrderLegComponent: React.FC<{leg: IAccountOpenOrderLegViewModel}> = observer((props) => {
-    const services = useServices();
-    return (
-        <>
-            <LegInfoBodyGridBox>
-                <LegInfoGridCellBox>{props.leg.quantity}</LegInfoGridCellBox>
-                <LegInfoSeparatorBox/>
-                <LegInfoGridCellBox>{props.leg.optionType}</LegInfoGridCellBox>
-                <LegInfoSeparatorBox/>
-                <LegInfoGridCellBox>{services.time.formatUserFriendlyMonthDay(props.leg.expirationDate)}</LegInfoGridCellBox>
-                <LegInfoSeparatorBox/>
-                <LegInfoGridCellBox>{props.leg.strikePrice}</LegInfoGridCellBox>
-            </LegInfoBodyGridBox>
-
-            <CenteredBodyGridCellBox>{props.leg.daysToExpiration}</CenteredBodyGridCellBox>
-            <RightBodyGridCellBox>{props.leg.tradingPrice.toFixed(2)}</RightBodyGridCellBox>
-            <RightBodyGridCellBox>{props.leg.bidPrice?.toFixed(2)}</RightBodyGridCellBox>
-            <RightBodyGridCellBox>{props.leg.askPrice?.toFixed(2)}</RightBodyGridCellBox>
-        </>
-    )
-})
-
-
-const OrderHeaderComponent: React.FC<{order: IAccountOpenOrderViewModel}> = observer(props => {
-    return (
-        <>
-            <GridBodyCellBox>
-                {`Order id: ${props.order.id}`}
-            </GridBodyCellBox>
-
-            <CenteredBodyGridCellBox>
-                {props.order.daysToExpiration}
-            </CenteredBodyGridCellBox>
-
-            <RightBodyGridCellBox>
-                {props.order.tradingPrice.toFixed(2)}
-            </RightBodyGridCellBox>
-
-            <RightBodyGridCellBox>
-                ?
-            </RightBodyGridCellBox>
-            <RightBodyGridCellBox>
-                ?
-            </RightBodyGridCellBox>
-        </>
-
-    )
-})
-
-const OrderComponent: React.FC<{order: IAccountOpenOrderViewModel}> = observer(props => {
-    const legs = props.order.legs;
-    return (
-        <BodyGridBox>
-            <OrderHeaderComponent order={props.order}/>
-            {legs.map((leg) => (<OrderLegComponent key={leg.symbol} leg={leg}/>))}
-        </BodyGridBox>
-    )
-})
-
-const UnderlyingSymbolOpenOrdersComponent: React.FC<{underlyingSymbol: string, openOrders: IAccountOpenOrderViewModel[]}> = observer((props) => {
-    const orders = props.openOrders.sort((o1, o2) => (o1.daysToExpiration ?? 0) - (o2.daysToExpiration ?? 0));
-    return (
-        <IonAccordion value={props.underlyingSymbol}>
-            <SymbolBox slot={"header"}>{props.underlyingSymbol}</SymbolBox>
-            <OrdersBox slot="content">
-                {orders.map(o => <OrderComponent key={o.id} order={o}/>)}
-            </OrdersBox>
-        </IonAccordion>
-
-    )
-})
-
-const HeaderComponent: React.FC = observer(() => {
-    return (
-        <HeaderGridBox>
-            <LegInfoHeaderGridBox>Symbol</LegInfoHeaderGridBox>
-            <CenteredHeaderGridCellBox>DTE</CenteredHeaderGridCellBox>
-            <RightHeaderGridCellBox>Trd Prc</RightHeaderGridCellBox>
-            <RightHeaderGridCellBox>Bid</RightHeaderGridCellBox>
-            <RightHeaderGridCellBox>Ask</RightHeaderGridCellBox>
-        </HeaderGridBox>
-    )
-})
 
 export const OpenPositionsPage: React.FC = observer(() => {
 
