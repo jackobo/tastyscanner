ef8d2c726491a95b3e7c03bba68d4a42370e5db7
Florin Iacob <florin.iacob@gmail.com>
2026-03-13 20:18:38 +0200

    Rename a folder.

--- src/app/services/navigator/routes/active-positions/active-positions.route.tsx
+++ src/app/services/navigator/routes/active-positions/active-positions.route.tsx
@@ -2,7 +2,7 @@ import {RootRouteModel} from "../../../../../framework/services/navigator/models
 import {IFrameworkServiceFactory} from "../../../../../framework/services/framework-service-factory.interface";
 
 import React from "react";
-import {ActivePositionsPage} from "../../../../pages/open-positions/active-positions.page";
+import {ActivePositionsPage} from "../../../../pages/active-positions/active-positions.page";
 
 export class ActivePositionsRoute extends RootRouteModel {
     constructor(services: IFrameworkServiceFactory) {
