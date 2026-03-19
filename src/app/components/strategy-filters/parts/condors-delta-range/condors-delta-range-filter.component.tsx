f79690d7ad9550a01728dcc60d63f2049dcfd68f
Florin Iacob <florin.iacob@gmail.com>
2026-03-19 16:39:18 +0200

    Change condors delta range to -5 to 5

--- src/app/components/strategy-filters/parts/condors-delta-range/condors-delta-range-filter.component.tsx
+++ src/app/components/strategy-filters/parts/condors-delta-range/condors-delta-range-filter.component.tsx
@@ -7,8 +7,8 @@ export const CondorsDeltaRangeFilterComponent: React.FC<{filters: IStrategyFilte
     const filters = props.filters;
     return (
         <RangeEditorComponent label="Condors delta range"
-                              min={-15}
-                              max={15}
+                              min={-5}
+                              max={5}
                               lower={filters.condorsMinDelta}
                               upper={filters.condorsMaxDelta}
                               tooltip={"Condors delta range: Only the Iron Condors with total delta within this range will be considered."}
