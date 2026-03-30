550e12cff0273b16784e211acae5836aee01bae8
Florin Iacob <florin.iacob@gmail.com>
2026-03-30 13:43:13 +0300

    Change column widths.

--- src/app/pages/active-positions/constants.ts
+++ src/app/pages/active-positions/constants.ts
@@ -5,7 +5,7 @@ export const UNDERLYING_SYMBOL_WIDTH = '160px';
 // DTE / PL% / PL / Mrk / TrdPrc / Bid /Ask / chevron
 export function getCommonColumnsTemplate(smallScreen: boolean) {
     if(smallScreen) {
-        return '45px repeat(6, 80px)'
+        return '45px 60px 80px repeat(4, 60px)'
     }
     return 'repeat(7, 1fr)'
 }
